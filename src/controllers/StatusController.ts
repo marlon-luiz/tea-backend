4f71343afffcfd45d06c14b0915bfba9984a4ce8
Emerson Brancher <Brancher81@gmail.com>
2019-10-23 00:43:09 -0200

    StatusController

---
+++ src/controllers/StatusController.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from 'express'
+import _ from 'lodash'
+
+import Activity from '../schemas/Activity'
+
+class StatusController {
+
+  public async update (req: Request, res: Response): Promise<Response> {
+    const { id } = req.params
+    const { user } = req.headers
+    const errors = []
+
+    let activity = await Activity.findByPk(id, {
+      attributes: [
+        'id',
+        'title',
+        'description',
+        'start',
+        'end',
+        'repeat',
+        'repeatEvery',
+        'repeatOn',
+        'autistId',
+        'isConcluded'
+      ]
+    })
+
+    if (activity === null) {
+      errors.push('Não foi possível encontrar a atividade.')
+    }
+
+    if (errors.length) {
+      throw new Error(errors.join('\n'))
+    }
+
+    if (activity !== null) {
+      activity = await activity.update({
+        isConcluded: false,
+        updatedBy: user
+      })
+    }
+
+    return res.json(_.pick(activity, [
+      'id',
+      'title',
+      'description',
+      'start',
+      'end',
+      'repeat',
+      'repeatEvery',
+      'repeatOn',
+      'autistId',
+      'isConcluded'
+    ]))
+  }
+
+}
+
+export default new StatusController()
--- src/routes/activities.ts
+++ src/routes/activities.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 
 import ActivityController from '../controllers/ActivityController'
 import wrapAsync from '../wrapAsync'
+import StatusController from '../controllers/StatusController'
 
 const router = Router()
 
@@ -9,5 +10,6 @@ router.get('/:id(\\d+)', wrapAsync(ActivityController.find))
 router.post('/', wrapAsync(ActivityController.store))
 router.put('/:id(\\d+)', wrapAsync(ActivityController.update))
 router.delete('/:id(\\d+)', wrapAsync(ActivityController.remove))
+router.put('/:id(\\d+)/status', wrapAsync(StatusController.update))
 
 export default router
